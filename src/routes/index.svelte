9d62442d626342b57d605d3f097e4944235f0b7f
sito <sito@zgen.hu>
2021-11-09 00:05:07 +0100

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,2 @@
+<h1>A GUILD OF CURIOUS BUILDERS</h1>
+<p>Building ethereum protocols and web3 applications for DeFi and DAO communitites.</p>
---
+++ tailwind.config.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+	mode: 'jit',
+	purge: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		fontFamily: {
+			display: ['Red Hat Display', 'sans-serif'],
+			body: ['Inter var', 'Inter', 'sans-serif']
+		},
+		extend: {
+			colors: {
+				black: '#333333',
+				white: '#FBFBFB',
+				gray: '#252525',
+				blue: {
+					light: '#7577FA',
+					medium: '#5053F2',
+					dark: '#3C3FDD'
+				}
+			}
+		}
+	},
+
+	container: {
+		center: true,
+		padding: { DEFAULT: '2rem', sm: '3rem', lg: '4rem' }
+	},
+
+	plugins: []
+};
