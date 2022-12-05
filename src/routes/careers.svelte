239172aac854256c7eacc965eaa8259578d96c42
valid <valid@zgen.hu>
2022-12-05 14:40:51 +0100

    feat: add `/careers` path that redirects to notion

---
+++ src/routes/careers.svelte
@@ -0,0 +1,8 @@
+<script context="module">
+	export async function load() {
+		return {
+			status: 302,
+			redirect: "https://guildxyz.notion.site/Careers-at-ZGEN-7ffd822c4d7749cb9c1adb525c858ae1"
+		};
+	}
+</script>
